26fc92d2140f966edaf52cc8e086072efd3e23d9
(no author) <(no author)@1c18d0e4-b14a-0410-8b7b-fc87120d1eea>
2003-09-11 14:52:05 +0000

    This commit was manufactured by cvs2svn to create tag 'Root-of-mrbs_MDB'.

---
+++ mrbs/tags/Root-of-mrbs_MDB/web/admin.php
@@ -0,0 +1,138 @@
+<?php
+
+# $Id$
+
+require_once "grab_globals.inc.php";
+include "config.inc";
+include "functions.inc";
+include "$dbsys.inc";
+include "mrbs_auth.inc";
+
+#If we dont know the right date then make it up 
+if(!isset($day) or !isset($month) or !isset($year))
+{
+	$day   = date("d");
+	$month = date("m");
+	$year  = date("Y");
+}
+
+if (empty($area))
+{
+    $area = get_default_area();
+}
+
+if(!getAuthorised(getUserName(), getUserPassword(), 2))
+{
+	showAccessDenied($day, $month, $year, $area);
+	exit();
+}
+
+print_header($day, $month, $year, isset($area) ? $area : "");
+
+// If area is set but area name is not known, get the name.
+if (isset($area))
+{
+	if (empty($area_name))
+	{
+		$res = sql_query("select area_name from mrbs_area where id=$area");
+    	if (! $res) fatal_error(0, sql_error());
+		if (sql_count($res) == 1)
+		{
+			$row = sql_row($res, 0);
+			$area_name = $row[0];
+		}
+		sql_free($res);
+	} else {
+		$area_name = unslashes($area_name);
+	}
+}
+?>
+
+<h2><?php echo $vocab['administration'] ?></h2>
+
+<table border=1>
+<tr>
+<th><center><b><?php echo $vocab['areas'] ?></b></center></th>
+<th><center><b><?php echo $vocab['rooms'] ?> <?php if(isset($area)) { echo $vocab['in'] . " " .
+  htmlspecialchars($area_name); }?></b></center></th>
+</tr>
+
+<tr>
+<td>
+<?php 
+# This cell has the areas
+$res = sql_query("select id, area_name from mrbs_area order by area_name");
+if (! $res) fatal_error(0, sql_error());
+
+if (sql_count($res) == 0) {
+	echo $vocab['noareas'];
+} else {
+	echo "<ul>";
+	for ($i = 0; ($row = sql_row($res, $i)); $i++) {
+		$area_name_q = urlencode($row[1]);
+		echo "<li><a href=\"admin.php?area=$row[0]&area_name=$area_name_q\">"
+			. htmlspecialchars($row[1]) . "</a> (<a href=\"edit_area_room.php?area=$row[0]\">" . $vocab['edit'] . "</a>) (<a href=\"del.php?type=area&area=$row[0]\">" .  $vocab['delete'] . "</a>)\n";
+	}
+	echo "</ul>";
+}
+?>
+</td>
+<td>
+<?php
+# This one has the rooms
+if(isset($area)) {
+	$res = sql_query("select id, room_name, description, capacity from mrbs_room where area_id=$area order by room_name");
+	if (! $res) fatal_error(0, sql_error());
+	if (sql_count($res) == 0) {
+		echo $vocab['norooms'];
+	} else {
+		echo "<ul>";
+		for ($i = 0; ($row = sql_row($res, $i)); $i++) {
+			echo "<li>" . htmlspecialchars($row[1]) . "(" . htmlspecialchars($row[2])
+			. ", $row[3]) (<a href=\"edit_area_room.php?room=$row[0]\">" .  $vocab['edit'] . "</a>) (<a href=\"del.php?type=room&room=$row[0]\">" .  $vocab['delete'] . "</a>)\n";
+		}
+		echo "</ul>";
+	}
+} else {
+	echo $vocab['noarea'];
+}
+
+?>
+
+</tr>
+<tr>
+<td>
+<h3 ALIGN=CENTER><?php echo $vocab['addarea'] ?></h3>
+<form action=add.php method=post>
+<input type=hidden name=type value=area>
+
+<TABLE>
+<TR><TD><?php echo $vocab['name'] ?>:       </TD><TD><input type=text name=name></TD></TR>
+</TABLE>
+<input type=submit value="<?php echo $vocab['addarea'] ?>">
+</form>
+</td>
+
+<td>
+<?php if(isset($area)) { ?>
+<h3 ALIGN=CENTER><?php echo $vocab['addroom'] ?></h3>
+<form action=add.php method=post>
+<input type=hidden name=type value=room>
+<input type=hidden name=area value=<?php echo $area; ?>>
+
+<TABLE>
+<TR><TD><?php echo $vocab['name'] ?>:       </TD><TD><input type=text name=name></TD></TR>
+<TR><TD><?php echo $vocab['description'] ?></TD><TD><input type=text name=description></TD></TR>
+<TR><TD><?php echo $vocab['capacity'] ?>:   </TD><TD><input type=text name=capacity></TD></TR>
+</TABLE>
+<input type=submit value="<?php echo $vocab['addroom'] ?>">
+</form>
+<?php } else { echo "&nbsp;"; }?>
+</td>
+</tr>
+</table>
+
+<br>
+<?php echo $vocab['browserlang'] . " " . $HTTP_ACCEPT_LANGUAGE . " " . $vocab['postbrowserlang'] ; ?>
+
+<?php include "trailer.inc" ?>
---
+++ mrbs/tags/Root-of-mrbs_MDB/web/edit_area_room.php
@@ -0,0 +1,110 @@
+<?php
+// $Id$
+
+require_once "grab_globals.inc.php";
+include "config.inc";
+include "functions.inc";
+include "$dbsys.inc";
+include "mrbs_auth.inc";
+
+#If we dont know the right date then make it up
+if(!isset($day) or !isset($month) or !isset($year))
+{
+	$day   = date("d");
+	$month = date("m");
+	$year  = date("Y");
+}
+
+if(!getAuthorised(getUserName(), getUserPassword(), 2))
+{
+	showAccessDenied($day, $month, $year, $area);
+	exit();
+}
+
+// Done changing area or room information?
+if (isset($change_done))
+{
+	if (!empty($room)) // Get the area the room is in
+	{
+		$area = sql_query1("SELECT area_id from mrbs_room where id=$room");
+	}
+	Header("Location: admin.php?day=$day&month=$month&year=$year&area=$area");
+	exit();
+}
+
+print_header($day, $month, $year, isset($area) ? $area : "");
+
+?>
+
+<h2><?php echo $vocab["editroomarea"] ?></h2>
+
+<table border=1>
+
+<?php
+if(!empty($room)) {
+	if (isset($change_room))
+	{
+		if (empty($capacity)) $capacity = 0;
+		$sql = "UPDATE mrbs_room SET room_name='" . slashes($room_name)
+			. "', description='" . slashes($description)
+			. "', capacity=$capacity WHERE id=$room";
+		if (sql_command($sql) < 0)
+			fatal_error(0, $vocab['update_room_failed'] . sql_error());
+	}
+
+	$res = sql_query("SELECT * FROM mrbs_room WHERE id=$room");
+	if (! $res) fatal_error(0, $vocab['error_room'] . $room . $vocab['not_found']);
+	$row = sql_row_keyed($res, 0);
+	sql_free($res);
+?>
+<h3 ALIGN=CENTER><?php echo $vocab["editroom"] ?></h3>
+<form action="edit_area_room.php" method="post">
+<input type=hidden name="room" value="<?php echo $row["id"]?>">
+<CENTER>
+<TABLE>
+<TR><TD><?php echo $vocab["name"] ?>:       </TD><TD><input type=text name="room_name" value="<?php
+echo htmlspecialchars($row["room_name"]); ?>"></TD></TR>
+<TR><TD><?php echo $vocab["description"] ?></TD><TD><input type=text name=description value="<?php
+echo htmlspecialchars($row["description"]); ?>"></TD></TR>
+<TR><TD><?php echo $vocab["capacity"] ?>:   </TD><TD><input type=text name=capacity value="<?php
+echo $row["capacity"]; ?>"></TD></TR>
+</TABLE>
+<input type=submit name="change_room"
+value="<?php echo $vocab["change"] ?>">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<input type=submit name="change_done" value="<?php echo $vocab["backadmin"] ?>">
+</CENTER>
+</form>
+<?php } ?>
+
+<?php
+if(!empty($area))
+{
+	if (isset($change_area))
+	{
+		$sql = "UPDATE mrbs_area SET area_name='" . slashes($area_name)
+			. "' WHERE id=$area";
+		if (sql_command($sql) < 0)
+			fatal_error(0, $vocab['update_area_failed'] . sql_error());
+	}
+
+	$res = sql_query("SELECT * FROM mrbs_area WHERE id=$area");
+	if (! $res) fatal_error(0, $vocab['error_area'] . $area . $vocab['not_found']);
+	$row = sql_row_keyed($res, 0);
+	sql_free($res);
+?>
+<h3 ALIGN=CENTER><?php echo $vocab["editarea"] ?></h3>
+<form action="edit_area_room.php" method="post">
+<input type=hidden name="area" value="<?php echo $row["id"]?>">
+<CENTER>
+<TABLE>
+<TR><TD><?php echo $vocab["name"] ?>:       </TD><TD><input type=text name="area_name" value="<?php
+echo htmlspecialchars($row["area_name"]); ?>"></TD></TR>
+</TABLE>
+<input type=submit name="change_area"
+value="<?php echo $vocab["change"] ?>">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<input type=submit name="change_done" value="<?php echo $vocab["backadmin"] ?>">
+</CENTER>
+</form>
+<?php } ?>
+</TABLE>
+<?php include "trailer.inc" ?>
